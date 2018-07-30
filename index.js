a63df57490a21c2b8f9b32cacb43e232f90e9a51
Paris Tuzun <paristuzun@gmail.com>
2018-07-30 23:57:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,10 +10,11 @@ function theBeatlesPlay(musicians, instruments) {
 
 function johnLennonFacts(facts) {
   var jfacts = [
-    
-    
-    
-    ];
+     "He was the last Beatle to learn to drive",
+     "He was never a vegetarian",
+     "He was a choir boy and boy scout",
+     "He hated the sound of his own voice"
+     ];
 }
 
 function iLoveTheBeatles(num) {
