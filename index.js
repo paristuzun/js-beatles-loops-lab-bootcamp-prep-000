73b2b51bdad440de427bb8794afb329087c3747b
Paris Tuzun <paristuzun@gmail.com>
2018-07-30 23:54:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function theBeatlesPlay(musicians, instruments) {
   var array = [];
   
-  for(i = 0; i < musicians.length; i++) {
+  for(let i = 0; i < musicians.length; i++) {
     array.push(`${musicians[i]} plays ${instruments[i]}`)
   }
   return array;
