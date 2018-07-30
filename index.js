5ad249f31fc97d3817a10b5e22f43def26b15d6c
Paris Tuzun <paristuzun@gmail.com>
2018-07-30 23:53:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ function theBeatlesPlay(musicians, instruments) {
   var array = [];
   
   for(i = 0; i < musicians.length; i++) {
-    array.push(`${musicians[i]} plays ${instruments[i]}`);
+    array.push(`${musicians[i]} plays ${instruments[i]}`)
   }
+  return array;
 }
 
 function johnLennonFacts(facts) {
